6ae4a377b2cfe042e4c4eca948423ca0e9155624
vvduong2k2 <duongnguyen2002.bmt@gmail.com>
2024-04-12 21:21:21 +0700

    edit filter category

--- frontend/src/component/filterQueryResultOptions/CategoryFilterComponent.js
+++ frontend/src/component/filterQueryResultOptions/CategoryFilterComponent.js
@@ -1,37 +1,47 @@
-import React, { useRef, useState } from "react";
 import { Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
 
 const CategoryFilterComponent = ({ setCategoriesFromFilter }) => {
   const { categories } = useSelector((state) => state.getCategories);
   const [selectedCategories, setSelectedCategories] = useState([]);
   const myRefs = useRef([]);
 
-  const selectCategory = (e, category) => {
-    setCategoriesFromFilter((items) => ({
-      ...items,
-      [category.name]: e.target.checked,
-    }));
-
-    const selectedMainCategory = category.name.split("/")[0];
-    const indexesOfMainCategory = categories
-      .filter((item) => selectedMainCategory === item.name.split("/")[0])
-      .map((item, idx) => idx);
-
-    const updatedCategories = e.target.checked
-      ? [...selectedCategories, selectedMainCategory]
-      : selectedCategories.slice(0, -1);
-
-    setSelectedCategories(updatedCategories);
-
-    myRefs.current.forEach((ref, idx) => {
-      ref.disabled = !updatedCategories.some((cat) =>
-        indexesOfMainCategory.includes(idx)
-      );
+  const selectCategory = (e, category, idx) => {
+    setCategoriesFromFilter((items) => {
+      return { ...items, [category.name]: e.target.checked };
     });
 
-    if (updatedCategories.length === 0) {
-      window.location.href = "/product-list";
+    const selectedMainCategory = category.name.split("/")[0];
+    const allCategories = myRefs.current.map((_, id) => ({
+      name: categories[id].name,
+      idx: id,
+    }));
+    const indexesOfMainCategory = allCategories
+      .filter((item) => selectedMainCategory === item.name.split("/")[0])
+      .map((item) => item.idx);
+
+    if (e.target.checked) {
+      setSelectedCategories((old) => {
+        const updatedCategories = [...old, selectedMainCategory];
+        myRefs.current.forEach((_, idx) => {
+          if (!indexesOfMainCategory.includes(idx))
+            myRefs.current[idx].disabled = false; // Thay đổi từ true sang false để enable
+        });
+        return updatedCategories;
+      });
+    } else {
+      setSelectedCategories((old) => {
+        const updatedCategories = [...old];
+        updatedCategories.pop();
+        if (updatedCategories.length === 0)
+          window.location.href = "/product-list";
+        myRefs.current.forEach((_, idx2) => {
+          if (allCategories.length === 1 || updatedCategories.length === 1)
+            myRefs.current[idx2].disabled = false;
+        });
+        return updatedCategories;
+      });
     }
   };
 
@@ -46,7 +56,7 @@ const CategoryFilterComponent = ({ setCategoriesFromFilter }) => {
                 ref={(el) => (myRefs.current[idx] = el)}
                 type="checkbox"
                 isValid
-                onChange={(e) => selectCategory(e, category)}
+                onChange={(e) => selectCategory(e, category, idx)}
               />
               <Form.Check.Label style={{ cursor: "pointer" }}>
                 {category.name}
