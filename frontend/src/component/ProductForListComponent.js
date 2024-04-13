ee344621dc358f2eeafe49491968ac5d6224ad81
Nguyen Quoc Kha <quockha081122@gmail.com>
2024-04-13 14:49:40 +0700

    productlistcomp

--- frontend/src/component/ProductForListComponent.js
+++ frontend/src/component/ProductForListComponent.js
@@ -6,6 +6,7 @@ const formatter = new Intl.NumberFormat("vi-VN", {
   style: "currency",
   currency: "VND",
 });
+
 const ProductForListComponent = ({
   productId,
   name,
@@ -16,29 +17,37 @@ const ProductForListComponent = ({
   reviewsNumber,
 }) => {
   return (
-    <Card style={{ marginTop: "30px", marginBottom: "50px" }}>
+    <Card style={{ marginBottom: "30px", boxShadow: "0 4px 8px rgba(0,0,0,0.1)" }}>
       <Row>
-        <Col lg={5}>
+        <Col lg={5} md={12} sm={12}>
           <Card.Img
             crossOrigin="anonymous"
             variant="top"
             src={images[0] ? images[0].path : ""}
+            style={{ height: "100%", objectFit: "cover" }}
           />
         </Col>
-        <Col lg={7}>
+        <Col lg={7} md={12} sm={12}>
           <Card.Body>
             <Card.Title>{name}</Card.Title>
             <Card.Text>{description}</Card.Text>
-            <Card.Text>
-              <Rating readonly size={20} initialValue={rating} /> (
-              {reviewsNumber})
-            </Card.Text>
-            <Card.Text className="h4">
-              {formatter.format(price)}{" "}
-              <LinkContainer to={`/product-details/${productId}`}>
-                <Button variant="danger">Xem Sản Phẩm</Button>
-              </LinkContainer>
-            </Card.Text>
+            <Row>
+              <Col xs={6}>
+                <Card.Text>
+                  <Rating readonly size={20} initialValue={rating} /> ({reviewsNumber})
+                </Card.Text>
+              </Col>
+              <Col xs={6} className="text-right">
+                <Card.Text className="h4">{formatter.format(price)}</Card.Text>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <LinkContainer to={`/product-details/${productId}`}>
+                  <Button variant="danger" block>Xem Sản Phẩm</Button>
+                </LinkContainer>
+              </Col>
+            </Row>
           </Card.Body>
         </Col>
       </Row>
